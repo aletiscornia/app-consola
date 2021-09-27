583d912904b2a952ed56edd74863b32270cd502d
Alejandro Tiscornia <adbadthbadn@gmail.com>
2021-09-26 21:57:29 -0300

    inicio de proyecto

---
+++ app.js
@@ -0,0 +1,15 @@
+require('colors');
+const { mostrarMenu } = require('./helpers/mensajes');
+
+
+console.clear();
+
+
+const main = async() => {
+
+    console.log('Hola Mundo');
+
+    mostrarMenu();
+}
+
+main();

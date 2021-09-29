93e5b60cf81a35ef066edd1582265376002b77dc
Alejandro Tiscornia <adbadthbadn@gmail.com>
2021-09-28 23:51:01 -0300

    Agregando opciones a la lista de tareas

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 require('colors');
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 
 
 console.clear();
 
 
 const main = async() => {
-
     console.log('Hola Mundo');
 
     let opt = '';
 
     do {
-        
-        opt = await mostrarMenu();  
-        console.log(opt);
-        
-        if(opt!== '0' ) await pausa();
+        opt = await inquirerMenu();  
+        console.log({opt});
+
+       await pausa();
 
     } while ( opt !== '0' );
 

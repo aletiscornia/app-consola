4d5ba4c3afaf64723aa40bde112a11087fa5d7b3
Alejandro Tiscornia <adbadthbadn@gmail.com>
2021-10-19 20:58:20 -0300

    listar tareas

--- models/tareas.js
+++ models/tareas.js
@@ -38,6 +38,25 @@ class Tareas{
 
     }
 
+    listadCompleto() {
+
+        this.listadoArr.forEach( (tarea, indice) => {
+           
+            console.log();
+            //para que el incice no arranque en 0 le sumamos 1
+            const idx = `${indice + 1}`.green;
+            //imprimimos el nombre de la tarea desestructurando el arreglo
+            const { desc, completadoEn } = tarea;
+            
+            const estado = ( completadoEn ) //ternario(abrebiacion del if)
+                                ?'Completada'.green
+                                :'Pendiente'.red;
+            console.log(`${idx} ${desc} :: ${estado}`);                    
+
+        })
+
+    }
+
 
 }
 
